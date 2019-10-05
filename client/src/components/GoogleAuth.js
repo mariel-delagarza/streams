06d17316c74e94cb72027846e4e8f553f680dba7
mariel-delagarza <mariel.code@gmail.com>
2019-10-05 17:15:51 -0400

    Added code to call gapi inside the browser, utilize our given clientId, and request the proper scope

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: '315665693019-4jgb6jb55uotts0emvlcc2h9mflitsq8.apps.googleusercontent.com',
+        scope: 'email'
+      });
+    }); 
+  }
+
   render() {
     return <div>GoogleAuth</div>;
   }
