ae3b7752d355f3279c59fd487ff792db58ee35f0
mariel-delagarza <mariel.code@gmail.com>
2019-10-06 08:52:13 -0400

    Create helper method renderAuthButton using a conditional to preturn <div>s based on whether the user is signed in

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
@@ -11,6 +13,16 @@ class GoogleAuth extends React.Component {
         this.setState({ isSignedIn: this.auth.isSignedIn.get() })
       });
     });  
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know if we are signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!</div>
+    } else {
+      return <div>I am not signed in</div>;
+    }
   }
 
   render() {
