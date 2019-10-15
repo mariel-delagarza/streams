0fed896cb3210e1d7f2cdf438023a536bf9e36a8
Mariel <mariel.code@gmail.com>
2019-10-15 20:25:21 +0000

    Wire up StreamList.js

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
-const StreamList = () => {
-  return <div>StreamList</div>;
-};
+class StreamList extends React.Commponent {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+
+  render() {
+    return <div>StreamList</div>;
+  }
+}
 
 
-export default StreamList;
+
+export default connect(null, { fetchStreams })(StreamList);
