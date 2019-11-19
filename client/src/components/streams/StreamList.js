19dca26a3910dba5163e45059d691aa44b9d7f7b
mariel-delagarza <mariel.code@gmail.com>
2019-11-19 06:54:44 -0500

    Add renderList function which adds a camera icon and the stream description

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -7,6 +7,20 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
+  renderList() {
+    return this.props.stream.map(stream => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
     return <div>StreamList</div>;
   }
