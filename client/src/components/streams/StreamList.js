0182d61175dc47cf1671126848d4761c8b998e17
mariel-delagarza <mariel.code@gmail.com>
2019-11-20 06:06:36 -0500

    Move location of helpermethod call so semantic-ui will style correctly

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -26,12 +26,12 @@ class StreamList extends React.Component {
     return this.props.streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
             <div className="description">{stream.description}</div>
           </div>
-          {this.renderAdmin(stream)}
         </div>
       );
     });
