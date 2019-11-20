961fb5180f7e0939b369e4a122804fba2ce3e074
mariel-delagarza <mariel.code@gmail.com>
2019-11-20 06:03:56 -0500

    Add renderAdmin helper method to renderList function

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -22,6 +22,7 @@ class StreamList extends React.Component {
             {stream.title}
             <div className="description">{stream.description}</div>
           </div>
+          {this.renderAdmin(stream)}
         </div>
       );
     });
