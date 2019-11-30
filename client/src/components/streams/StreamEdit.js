409e6bc7192f179a489d1220a7a474f758c32d08
mariel-delagarza <mariel.code@gmail.com>
2019-11-30 17:13:11 -0500

    Import fetchStream, change to class component, utilize fetchStream inside componentDidMount

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { fetchStream} from '../../actions';
 
-const StreamEdit = (props) => {
-  return <div>StreamEdit</div>;
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    console.log(this.props);
+    return <div>StreamEdit</div>;
+  }
 };
 
 
@@ -10,4 +18,4 @@ const mapStateToProps = (state, ownProps) => {
   return {stream: state.streams[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps)(StreamEdit);
+export default connect(mapStateToProps, {fetchStream})(StreamEdit);
