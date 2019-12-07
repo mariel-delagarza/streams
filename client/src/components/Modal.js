b8eb13537acf6bf0ac99d8daa76c303456019e10
mariel-delagarza <mariel.code@gmail.com>
2019-12-07 17:12:35 -0500

    Replace hardcoded text with prop use

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -7,7 +7,7 @@ const Modal = props => {
   return ReactDOM.createPortal(
     <div onClick={() => history.push('/')} className="ui dimmer modals visible active">
       <div onClick={(e) => e.stopPropagation()} className="ui standard modal visible active">
-        <div className="header">Delete Stream</div>
+        <div className="header">{props.title}</div>
         <div className="content">
           Are you sure you want to delete this stream?
         </div>
