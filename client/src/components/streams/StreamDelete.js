25e26924f12e60c68751f11158471807dde56dc5
mariel-delagarza <mariel.code@gmail.com>
2019-12-07 19:48:11 -0500

    Put actions inside helper method, remove const assignment and turn to return statement, update actions prop to call helper method

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -3,12 +3,14 @@ import Modal from '../Modal';
 import history from '../../history';
 
 class StreamDelete extends React.Component {
-  const actions = (
-    <React.Fragment>
-      <button className="ui button negative">Delete</button>
-      <button className="ui button">Cancel</button>
-    </React.Fragment>
-  );
+  renderActions() {
+    return (
+      <React.Fragment>
+        <button className="ui button negative">Delete</button>
+        <button className="ui button">Cancel</button>
+      </React.Fragment>
+    );
+  }
 
   render() {
     return (
@@ -17,7 +19,7 @@ class StreamDelete extends React.Component {
         <Modal 
           title="Delete Stream"
           content="Are you sure you want to delete this stream?"
-          actions={actions} 
+          actions={this.renderActions()} 
           onDismiss={() => history.push('/')} />
       </div>
     );
