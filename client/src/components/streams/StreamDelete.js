0263325fb8bad59e0a32900f30f50ca5a9024d48
mariel-delagarza <mariel.code@gmail.com>
2019-12-08 18:32:15 -0500

    Wire up delete button

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Modal from '../Modal';
 import history from '../../history';
-import { fetchStream } from '../../actions';
+import { fetchStream, deleteStream } from '../../actions';
 
 class StreamDelete extends React.Component {
   componentDidMount() {
@@ -13,9 +13,10 @@ class StreamDelete extends React.Component {
 
 
   renderActions() {
+    const { id } = this.props.match.params
     return (
       <React.Fragment>
-        <button className="ui button negative">Delete</button>
+        <button onClick={() => this.props.deleteStream(id) } className="ui button negative">Delete</button>
         <Link to="/" className="ui button">Cancel</Link>
       </React.Fragment>
     );
@@ -46,4 +47,4 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] }
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamDelete);
+export default connect(mapStateToProps, { fetchStream, deleteStream })(StreamDelete);
