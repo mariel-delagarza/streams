e4f7f628a8a379848d4cc7a0deb0fde9e2ad461c
mariel-delagarza <mariel.code@gmail.com>
2019-12-07 19:46:39 -0500

    Complete render method by wrapping return statement, update formatting

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -11,21 +11,18 @@ class StreamDelete extends React.Component {
   );
 
   render() {
-    
+    return (
+      <div>
+        StreamDelete
+        <Modal 
+          title="Delete Stream"
+          content="Are you sure you want to delete this stream?"
+          actions={actions} 
+          onDismiss={() => history.push('/')} />
+      </div>
+    );
   }
-
-
-  return (
-    <div>
-      StreamDelete
-      <Modal 
-        title="Delete Stream"
-        content="Are you sure you want to delete this stream?"
-        actions={actions} 
-        onDismiss={() => history.push('/')} />
-    </div>
-  );
-};
+}
 
 
 export default StreamDelete;
