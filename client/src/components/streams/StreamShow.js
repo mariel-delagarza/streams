5073d185855a32445e625b9a37519d2364d94df2
mariel-delagarza <mariel.code@gmail.com>
2020-01-12 16:27:17 -0500

    add import fetchStream from actions

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import flv from 'flv.js';
 import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
 const StreamShow = () => {
   return <div>StreamShow</div>;
